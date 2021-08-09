542dc6a3c79ea034e9cb3aa0a4272d24ecff40ba
JrCampos007 <camposmj@oregonstate.edu>
2021-08-09 11:59:11 -0700

    created another function for adding length plus first value

--- function_basic2.py
+++ function_basic2.py
@@ -10,3 +10,8 @@ def print_and_return(a):
     print(a[0])
     return(a[1])
 print(print_and_return([1,2]))
+# First Plus Length
+def first_plus_length(a):
+    y = a[0] + len(a)
+    return y
+print(first_plus_length([5,2,3,4,6,7]))
