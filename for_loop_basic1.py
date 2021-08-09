77b93019c0f856a3b66851e76301f4fddc92b059
JrCampos007 <camposmj@oregonstate.edu>
2021-08-09 10:08:21 -0700

    Adding the coding dojo print function for multiples of 5 and 10

--- for_loop_basic1.py
+++ for_loop_basic1.py
@@ -5,6 +5,13 @@ for x in range(151):
 for x in range(5,1001,5):
     print(x)
 # Counting, the Dojo Way
+for x in range (1,101):
+    if x % 10 == 0:
+        print("Coding Dojo")
+    elif x % 5 == 0:
+        print("Coding")
+    else:
+        print(x)
 # Whoa. That Sucker's Huge
 # Countdown by Fours
 # Flexible Counter
