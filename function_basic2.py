8d476b44a9f55a637afd93b119e408148e8dfd48
JrCampos007 <camposmj@oregonstate.edu>
2021-08-09 11:56:24 -0700

    added print first value and return second

--- function_basic2.py
+++ function_basic2.py
@@ -5,3 +5,8 @@ def Countdown(a):
         lis.append(x)
     return lis
 print(Countdown(20))
+# Print and Return
+def print_and_return(a):
+    print(a[0])
+    return(a[1])
+print(print_and_return([1,2]))
