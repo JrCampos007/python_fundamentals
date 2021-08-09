30a68e641187bccffc17b1c8084edb4bb89d88ca
JrCampos007 <camposmj@oregonstate.edu>
2021-08-09 12:20:36 -0700

    added new function

--- function_basic2.py
+++ function_basic2.py
@@ -15,3 +15,17 @@ def first_plus_length(a):
     y = a[0] + len(a)
     return y
 print(first_plus_length([5,2,3,4,6,7]))
+# Values greater than second
+def values_greater_than_second(a):
+    lis=[]
+    count= 0
+    if len(a) < 2:
+        return False
+    for x in range (len(a)):
+        if a[x] > a[1]:
+            print(a[x], a[1])
+            lis.append(a[x])
+            count = count + 1
+    print(count)
+    return(lis)
+print(values_greater_than_second([5,2,3,1,4,5]))
