6948a9ce6e3859752d8d205ff62142e7bfb2c72f
JrCampos007 <camposmj@oregonstate.edu>
2021-08-17 09:33:49 -0700

    two min and max are provided

--- python_functions_intermediate1.py
+++ python_functions_intermediate1.py
@@ -20,3 +20,8 @@ def randInt(min=0, max=100):
 print('integer between 60 to 100: ', randInt(min=50), '\n')
 
 # If both a min and max number are provided, the function should return a random integer between those 2 values.
+import random
+def randInt(min=0, max=100):
+    num = random.random() * (max-min) + min
+    return round(num)
+print('integer between 50 and 500: ', randInt(min=50, max=500), '\n')
