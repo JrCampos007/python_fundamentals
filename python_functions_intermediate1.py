cd4831b04db07bf49d1a144821d2c3874e70eb99
JrCampos007 <camposmj@oregonstate.edu>
2021-08-17 09:37:14 -0700

    added a couple edge cases

--- python_functions_intermediate1.py
+++ python_functions_intermediate1.py
@@ -25,3 +25,25 @@ def randInt(min=0, max=100):
     num = random.random() * (max-min) + min
     return round(num)
 print('integer between 50 and 500: ', randInt(min=50, max=500), '\n')
+
+###########################################################################################
+# # Added a couple edge cases
+###########################################################################################
+# 1
+import random
+def randInt(min=0, max=100):
+    if min > max:
+        print('you are not very good at this')
+        return
+    num = random.random() * (max-min) + min
+    return round(num)
+print('edge case min>max: ', randInt(min=10, max=5), '\n')
+# 2
+import random
+def randInt(min=0, max=100):
+    if max < 0:
+        print('you are not very good at this')
+        return
+    num = random.random() * (max-min) + min
+    return round(num)
+print('edge case max<0: ', randInt(min=10, max=-5), '\n')
