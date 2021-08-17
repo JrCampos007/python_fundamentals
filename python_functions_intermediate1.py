7a08cbb22965ba1f001c2a3f07598560e94cc690
JrCampos007 <camposmj@oregonstate.edu>
2021-08-17 09:23:26 -0700

    added function with only max number provided

--- python_functions_intermediate1.py
+++ python_functions_intermediate1.py
@@ -5,6 +5,10 @@ def randInt(min=0, max=100):
     return round(num)
 print('integer between 0 to 100: ', randInt(), '\n')
 # If only a max number is provided, the function should return a random integer between 0 and the max number.
-
+import random
+def randInt(min=0, max=100):
+    num = random.random() * max + min
+    return round(num)
+print('integer between 0 to 60: ', randInt(max=60), '\n')
 # If only a min number is provided, the function should return a random integer between the min number and 100
 # If both a min and max number are provided, the function should return a random integer between those 2 values.
